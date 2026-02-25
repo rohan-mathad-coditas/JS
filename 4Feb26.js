a822a1c0d0eaf17b864f22160dc2a4f675e7bbdf
Rohan Mathad <rohan.mathad@coditas.com>
2026-02-25 09:53:46 +0530

    correction

--- 4Feb26.js
+++ 4Feb26.js
@@ -2,4 +2,5 @@ const oddNumbers = [93, 27, 5];
 
 const evenNumbers = [2, 4, 6,8,10];
 
-const mergedArray = [oddNumbers, evenNumbers];
+const mergedArray = [...oddNumbers, ...evenNumbers];
+console.log(mergedArray);
